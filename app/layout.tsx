c70d75539cb660e2383405c89ac71f0c29451475
Oyenola Philip <oyenolaphilip89@gmail.com>
2025-12-04 20:54:14 +0100

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <script src="https://quge5.com/88/tag.min.js" data-zone="190402" async data-cfasync="false"></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5981344326362620" crossOrigin="anonymous"></script>
         <meta name="monetag" content="4422b58efc394603db03aba4126676ce"></meta> 
       </head>
